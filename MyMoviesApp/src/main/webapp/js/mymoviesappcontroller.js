4d2ec9ce5163681416f7e9ff470b32938fb8d4e2
Prateek Kakirwar <prateekkakirwar@gmail.com>
2014-07-01 13:37:08 -0700

    Assignment 7

--- MyMoviesApp/src/main/webapp/js/mymoviesappcontroller.js
+++ MyMoviesApp/src/main/webapp/js/mymoviesappcontroller.js
@@ -42,7 +42,60 @@ MyMoviesApp.controller('MyMoviesAppAddMovieController', function($scope, $http) 
 				$scope.httpStatus = status;
 				$scope.httpData = data;
 				$scope.errorStatus=false;
-				$scope.messageText="Created new user with ID "+data.user.id;				
+				$scope.messageText="Created new movie with ID "+data.user.id;				
+			})
+			.error(function(data, status) {
+				$scope.httpStatus = status;				
+				$scope.httpData = data;
+				$scope.errorStatus=true;
+				$scope.messageText=data.error.code+ " "+ data.error.message;
+			});
+	};
+});
+
+MyMoviesApp.controller('TheatreSearchController', function($scope, $http) {
+	$scope.theatres = [];
+
+	$scope.search = function() {
+		//remove current results
+		$scope.theatres = [];
+		$scope.messageText="";
+		
+		//search
+		$http.get('/MyMoviesApp/rest/theatres',		
+			{params: {
+				theatreName:$scope.searchTheatreName, 
+				zipCode:$scope.searchZipCode,
+				cityName:$scope.searchCityName,
+				stateName:$scope.searchStateName}})
+			.success(function(data, status) {
+				$scope.httpStatus = status;
+				$scope.httpData = data;
+				$scope.errorStatus=false;
+				$scope.messageText="Found "+data.length;
+				$scope.theatres = data;
+			})
+			.error(function(data, status) {
+				$scope.httpStatus = status;				
+				$scope.httpData = data;
+				$scope.errorStatus=true;
+				$scope.messageText=data.error.code+ " "+ data.error.message;
+			});		
+	};
+});
+
+MyMoviesApp.controller('MyMoviesAppAddTheatreController', function($scope, $http) {	
+	
+	$scope.create = function() {
+		$scope.messageText="";
+		
+		$http.post('/MyMoviesApp/rest/theatres',
+				{"theatre":{"theatreName":$scope.createTheatreName,"zipcode":$scope.createZipCode,"cityname":$scope.createCityName,"statename":$scope.createStateName}})
+			.success(function(data, status) {
+				$scope.httpStatus = status;
+				$scope.httpData = data;
+				$scope.errorStatus=false;
+				$scope.messageText="Created new theatre with ID "+data.user.id;				
 			})
 			.error(function(data, status) {
 				$scope.httpStatus = status;				
