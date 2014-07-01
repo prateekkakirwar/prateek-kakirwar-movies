ee56941db2981d131cc10227bbf897de6a4a0f0f
Prateek Kakirwar <prateekkakirwar@gmail.com>
2014-07-01 11:34:27 -0700

    Assignment 7

--- MyMoviesApp/src/main/webapp/js/mymoviesappcontroller.js
+++ MyMoviesApp/src/main/webapp/js/mymoviesappcontroller.js
@@ -1,5 +1,58 @@
 var MyMoviesApp = angular.module('MyMoviesApp', []);
 
+MyMoviesApp.controller('MovieSearchController', function($scope, $http) {
+	$scope.movies = [];
+
+	$scope.search = function() {
+		//remove current results
+		$scope.movies = [];
+		$scope.messageText="";
+		
+		//search
+		$http.get('/MyMoviesApp/rest/movies',		
+			{params: {
+				movieName:$scope.searchMovieName, 
+				releaseDate:$scope.searchReleaseDate,
+				rating:$scope.searchMovieRating,
+				genre:$scope.searchMovieGenre}})
+			.success(function(data, status) {
+				$scope.httpStatus = status;
+				$scope.httpData = data;
+				$scope.errorStatus=false;
+				$scope.messageText="Found "+data.length;
+				$scope.movies = data;
+			})
+			.error(function(data, status) {
+				$scope.httpStatus = status;				
+				$scope.httpData = data;
+				$scope.errorStatus=true;
+				$scope.messageText=data.error.code+ " "+ data.error.message;
+			});		
+	};
+});
+
+MyMoviesApp.controller('MyMoviesAppAddMovieController', function($scope, $http) {	
+	
+	$scope.create = function() {
+		$scope.messageText="";
+		
+		$http.post('/MyMoviesApp/rest/movies',
+				{"movie":{"movieName":$scope.createMovieName,"releaseDate":$scope.createReleaseDate,"rating":$scope.createRating,"genre":$scope.createGenre}})
+			.success(function(data, status) {
+				$scope.httpStatus = status;
+				$scope.httpData = data;
+				$scope.errorStatus=false;
+				$scope.messageText="Created new user with ID "+data.user.id;				
+			})
+			.error(function(data, status) {
+				$scope.httpStatus = status;				
+				$scope.httpData = data;
+				$scope.errorStatus=true;
+				$scope.messageText=data.error.code+ " "+ data.error.message;
+			});
+	};
+});
+
 MyMoviesApp.controller('UserSearchController', function($scope, $http) {
 	$scope.users = [];
 
